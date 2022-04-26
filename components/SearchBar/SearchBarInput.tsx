da7e52ac79b475cbc06297e5896d9641388ecfee
Andronomewalka <imozzart1@gmail.com>
2022-04-26 18:26:26 +0300

    fix weird (ReferenceError: Event is not defined) on node 14

--- components/SearchBar/SearchBarInput.tsx
+++ components/SearchBar/SearchBarInput.tsx
@@ -8,15 +8,15 @@ import { SearchBarInputType } from './types';
 import { SearchIcon } from './SearchIcon';
 import { useDebounce } from 'hooks/useDebounce';
 
-const submitEvent = new Event('submit', { cancelable: true, bubbles: true });
-
 export const SearchBarInput: FC<SearchBarInputType> = ({ onSubmit }) => {
   const formRef = useRef<HTMLFormElement>(null);
   const [input, setInput] = useState('');
 
   useDebounce(
     useCallback(() => {
-      formRef.current?.dispatchEvent(submitEvent);
+      formRef.current?.dispatchEvent(
+        new Event('submit', { cancelable: true, bubbles: true })
+      );
     }, []),
     input,
     350
