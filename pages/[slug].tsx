72c79be349e5a72e42e9cba7c999c678e415d725
Andronomewalka <imozzart1@gmail.com>
2022-04-26 17:36:12 +0300

    [slug] page fix

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,10 +2,10 @@ import type { NextPage, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import { serialize } from 'next-mdx-remote/serialize';
 import { motion } from 'framer-motion';
+import styled from 'styled-components';
 import rehypeHighlight from 'rehype-highlight';
 import { Mdx } from 'components/Mdx';
 import { client } from 'utils/client';
-import { SlugContentWrapper } from './styles';
 
 const SlugPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   name,
@@ -90,3 +90,42 @@ export async function getStaticPaths() {
     fallback: 'blocking',
   };
 }
+
+// customize md components
+export const SlugContentWrapper = styled.article`
+  line-height: 1.7;
+  padding: 0 1.25rem 1.25rem;
+
+  ul,
+  ol {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+  }
+
+  ul li::marker {
+    color: ${(props) => props.theme.palette.accent};
+    font-size: 1.5em;
+  }
+
+  ul {
+    list-style-type: disc;
+  }
+
+  pre {
+    margin: 1.35rem 0;
+  }
+
+  /* select only inline code (multiline is styled by rehype-highlight)*/
+  *:not(pre) > code {
+    padding: 0.2rem 0.35rem;
+    border-radius: ${(props) => props.theme.borderRadius};
+    background: ${(props) => props.theme.palette.gray2};
+    font-family: Consolas, monospace;
+  }
+
+  /* multiline code container */
+  .hljs {
+    border-radius: ${(props) => props.theme.borderRadius};
+  }
+`;
