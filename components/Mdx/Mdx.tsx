55a4b6370f402d547875695167e14ebd9d972fab
Andronomewalka <imozzart1@gmail.com>
2022-05-10 01:07:34 +0300

    double animation on mdx load fix

--- components/Mdx/Mdx.tsx
+++ components/Mdx/Mdx.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useRef } from 'react';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import useSWR from 'swr';
@@ -42,20 +42,21 @@ const components = {
 
 export const Mdx: FC<MdxType> = ({ url, prefetchedData }) => {
   const { data: response } = useSWR(url);
-  const [swrData, setSwrData] = useState<any>();
+  const [swrData, setSwrData] = useState<any>(prefetchedData);
+  const firstSWRRef = useRef(true);
 
+  // first time we show prefetchedData, so skip swrData update for prevent animation flickering
   useEffect(() => {
-    if (response?.data?.content) {
+    if (response?.data?.content && firstSWRRef.current) {
+      firstSWRRef.current = false;
+    } else if (response?.data?.content) {
       serialize(response.data.content, {
         mdxOptions: { rehypePlugins: [rehypeHighlight] },
       }).then((newData) => {
         setSwrData(newData);
       });
     }
-    return () => {
-      setSwrData(null);
-    };
-  }, [response]);
+  }, [response?.data?.content]);
 
-  return <MDXRemote {...(swrData ? swrData : prefetchedData)} components={components} />;
+  return <MDXRemote {...swrData} components={components} />;
 };
