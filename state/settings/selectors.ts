5f9a814e51bc4825de8255f995d814ec14f820f2
andronomewalka <imozzart1@gmail.com>
2022-07-10 13:39:42 +0300

    recoil selectors ts fix

--- state/settings/selectors.ts
+++ state/settings/selectors.ts
@@ -1,7 +1,8 @@
 import { DefaultValue, selector } from 'recoil';
 import { settingsState, settingsStateInitialValue } from './atom';
+import { ThemeType } from './types';
 
-export const settingsThemeState = selector({
+export const settingsThemeState = selector<ThemeType>({
   key: 'settingsThemeState',
   get: ({ get }) => get(settingsState).theme,
   set: ({ get, set }, newValue) =>
@@ -13,7 +14,7 @@ export const settingsThemeState = selector({
     ),
 });
 
-export const settingsAnimationsState = selector({
+export const settingsAnimationsState = selector<boolean>({
   key: 'settingsAnimationsState',
   get: ({ get }) => get(settingsState).isAnimationsEnabled,
   set: ({ get, set }, newValue) =>
@@ -25,7 +26,7 @@ export const settingsAnimationsState = selector({
     ),
 });
 
-export const settingsLevelIndicatorState = selector({
+export const settingsLevelIndicatorState = selector<boolean>({
   key: 'settingsLevelIndicatorState',
   get: ({ get }) => get(settingsState).isLevelIndicatorVisible,
   set: ({ get, set }, newValue) =>
